4c37406876379db4664ad2e8647205c402525c97
xinpeilu <icydolphin-ee@163.com>
2018-03-22 11:16:49 +0800

    增加pom的相关依赖

--- spring-demo/src/main/java/com/ctvit/App.java
+++ spring-demo/src/main/java/com/ctvit/App.java
@@ -1,13 +1,26 @@
 package com.ctvit;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * Hello world!
  *
  */
+@SpringBootApplication
+@RestController("/")
 public class App 
 {
+	@RequestMapping("hello")
+	public String hello(){
+		return "hello";
+	}
+	
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        SpringApplication.run(App.class, args);
     }
 }
